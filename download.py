5bc9c2d92cc44698b4db1404fddcc8533d85b9d3
--global <--global>
2020-12-12 13:06:31 -0600

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,8 @@
 import os
 
-os.system("kaggle datasets download -d greatgamedota/torchvision-modified")
-os.system("unzip -q torchvision-modified.zip")
-os.system("rm torchvision-modified.zip")
+# os.system("kaggle datasets download -d greatgamedota/torchvision-modified")
+# os.system("unzip -q torchvision-modified.zip")
+# os.system("rm torchvision-modified.zip")
 
 os.system("kaggle datasets download -d greatgamedota/nfl-impact-detection-train-images")
 os.system("unzip -q nfl-impact-detection-train-images.zip -d 'data/'")
